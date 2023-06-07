9c224ba48a4e37f2921a3e1ab3fc72c1427d2492
Katerina <118116963+NonStopIT17@users.noreply.github.com>
2023-06-07 09:47:15 +0300

    Home work 2/1
    +++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,54 +2,66 @@ import { Component } from "react";
 import { ContactList, Filter } from "./contacts/Contacts";
 import { GlobalStyle } from "./GlobalStyle.styled";
 import { NewContactForm } from "./new-contact/NewContact";
+import { v4 as uuidv4 } from 'uuid';
 
-export class App extends Component{
-    state = {
-      contacts: [
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-      ],
-      filter: '',
-    }
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-    render(){
-      return(
-        <div style={{padding: "20px"}}>
-            <GlobalStyle/>
-            <div>
-            <h1 style={{marginBottom: "20px"}}>Phonebook</h1>
-            <NewContactForm submitHandling = {this.submitHandling} />
+  submitHandling = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+    const newContact = {
+      id: uuidv4(),
+      name: name,
+      number: number,
+    };
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+    form.reset(); // Очищення форми
+  };
 
-            <h2 style={{marginBottom: "10px"}}>Contacts</h2>
-            <Filter setFilter={this.setFilter}/>
-            <ContactList contacts={this.state.contacts} filter={this.state.filter} deleteContact={this.deleteContact}/>
-          </div>
-        </div>
-      )
-    }
+  setFilter = (event) => {
+    const inputText = event.currentTarget.value;
+    this.setState({ filter: inputText });
+  };
 
-    submitHandling = event =>{
-      event.preventDefault();
-      const id = this.state.contacts.length + 1;
-      const form = event.currentTarget;
-      const name = form.elements.name.value;
-      const number = form.elements.number.value;
-      this.setState({
-        ...this.state,
-        contacts: [
-          ...this.state.contacts,
-          { id: 'id-'+id, name: name, number: number },
-        ],
-      });
-   }
-   setFilter = event =>{
-    const inpuText = event.currentTarget.value;
-    this.setState({filter: inpuText});
-   }
-   deleteContact = event =>{
+  deleteContact = (event) => {
     const targetId = event.currentTarget.id;
-    this.setState(prevState => ({contacts: prevState.contacts.filter(item => item.id !== targetId)}))
-   }
-};
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((item) => item.id !== targetId),
+    }));
+  };
+
+  render() {
+    return (
+      <div style={{ padding: "20px" }}>
+        <GlobalStyle />
+        <div>
+          <h1 style={{ marginBottom: "20px" }}>Phonebook</h1>
+          <NewContactForm submitHandling={this.submitHandling} />
+
+          <h2 style={{ marginBottom: "10px" }}>Contacts</h2>
+          <Filter setFilter={this.setFilter} />
+          <ContactList
+            contacts={this.state.contacts}
+            filter={this.state.filter}
+            deleteContact={this.deleteContact}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+
