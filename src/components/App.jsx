aed0210b986764ce970b60e74142196471f74d81
Katerina <118116963+NonStopIT17@users.noreply.github.com>
2023-06-13 08:53:49 +0300

    Home work 2/1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,18 @@ import { ContactList } from "../components/contacts/Contacts";
 import { GlobalStyle } from "./GlobalStyle.styled";
 import NewContactForm from "./NewContactForm/NewContactForm";
 import { v4 as uuidv4 } from "uuid";
+import Filter from "../components/filter/Filter";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [
-        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-      ],
-    };
-  }
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
 
   addContact = (contact) => {
     const { contacts } = this.state;
@@ -41,16 +40,27 @@ class App extends Component {
   };
 
   deleteContact = (id) => {
-    const { contacts } = this.state;
-    const updatedContacts = contacts.filter((contact) => contact.id !== id);
+    this.setState((prevState) => {
+      const updatedContacts = prevState.contacts.filter(
+        (contact) => contact.id !== id
+      );
 
-    this.setState({
-      contacts: updatedContacts,
+      return {
+        contacts: updatedContacts,
+      };
     });
   };
 
+  handleFilterChange = (value) => {
+    this.setState({ filter: value });
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
 
     return (
       <div style={{ padding: "20px" }}>
@@ -60,7 +70,12 @@ class App extends Component {
           <NewContactForm addContact={this.addContact} />
 
           <h2 style={{ marginBottom: "10px" }}>Contacts</h2>
-          <ContactList contacts={contacts} deleteContact={this.deleteContact} />
+          <Filter setFilter={this.handleFilterChange} />
+
+          <ContactList
+            contacts={filteredContacts}
+            deleteContact={this.deleteContact}
+          />
         </div>
       </div>
     );
@@ -68,6 +83,8 @@ class App extends Component {
 }
 
 export default App;
+
+
 
 
 
--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,52 +1,15 @@
 import React from "react";
 import { ContactItem, DeleteButton } from "../contacts/Contacts.styled";
 import PropTypes from "prop-types";
-import Filter from "../filter/Filter";
 
 class ContactList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filteredContacts: [],
-      filter: "",
-    };
-  }
-
-  componentDidMount() {
-    this.filterContacts();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevProps.contacts !== this.props.contacts ||
-      prevState.filter !== this.state.filter
-    ) {
-      this.filterContacts();
-    }
-  }
-
-  handleFilterChange = (value) => {
-    this.setState({ filter: value });
-  };
-
-  filterContacts = () => {
-    const { contacts } = this.props;
-    const { filter } = this.state;
-    const filtered = contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-    this.setState({ filteredContacts: filtered });
-  };
-
   render() {
-    const { filteredContacts } = this.state;
-    const { deleteContact } = this.props;
+    const { contacts, deleteContact } = this.props;
 
     return (
       <div>
-        <Filter setFilter={this.handleFilterChange} />
         <ul style={{ paddingLeft: "30px" }}>
-          {filteredContacts.map((contact) => (
+          {contacts.map((contact) => (
             <ContactItem key={contact.id}>
               <span style={{ marginRight: "10px" }}>
                 {contact.name}: {contact.number}
@@ -77,6 +40,8 @@ ContactList.propTypes = {
 };
 
 export { ContactList };
+
+
  
 
 
